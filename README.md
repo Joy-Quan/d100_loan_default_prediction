ad1a4affabfa5dfa232be52a9384ad2032de286e
Joy-Quan <heyjoy7788@gmail.com>
2025-12-13 00:13:07 +0800

    [init]

---
+++ README.md
@@ -0,0 +1,1 @@
+# D100: Loan Default Prediction
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="loan_prediction",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "lightgbm",
+        "pyarrow",  # for parquet
+        "dalex",    # for model interpretation
+    ],
+)
